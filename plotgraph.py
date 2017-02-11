e94b21e18027aa627b95fccec158d9dabf2a8dfe
Tibor Leupold <tibor.leupold@gmail.com>
2017-02-11 18:15:06 +0100

    Splitting lines into words. Adding words that are floats to number_in_line.

--- plotgraph.py
+++ plotgraph.py
@@ -1,5 +1,6 @@
 import sublime
 import sublime_plugin
+import re
 
 
 # The command class "ExampleCommand" can be called by view.run_command("example").
@@ -14,7 +15,8 @@ class MyFirstCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.insert(edit, 0, "This is my test")
 
-
+# http://stackoverflow.com/questions/354038/ \
+# how-do-i-check-if-a-string-is-a-number-float-in-python
 def is_number(s):
     try:
         float(s)
@@ -48,12 +50,18 @@ class ReturnSelectionCommand(sublime_plugin.TextCommand):
                 lines_in_selection = selection_str.split("\n")
                 print(lines_in_selection)
                 for line in lines_in_selection:
+                    numbers_in_line = []
                     # Only keeping lines that are not empty.
                     if line:
                         lines = lines + [line]
+                        # Split the line into "words". (http://stackoverflow.com/a/23720594/6771403)
+                        print(line)
+                        words_in_line = re.split("[, \-!?:]+", line)
+                        print("words = {0}".format(words_in_line))
+                        # Check if the word is a number. Only keep numbers. At least in the vector variable.
+                        for word in words_in_line:
+                            if is_number(word):
+                                numbers_in_line = numbers_in_line + [word]
+                        print("numbers_in_line = {0}".format(numbers_in_line))
+
         print(lines)
-
-
-        
-
-#test change for no res.
