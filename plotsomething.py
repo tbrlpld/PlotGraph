018de1e794b439c702bcc39488fab0fd673f4cf0
Tibor Leupold <tibor.leupold@gmail.com>
2017-02-12 17:18:52 +0100

    Chnaged from ast.literal_eval() to built-in eval().

--- plotsomething.py
+++ plotsomething.py
@@ -6,13 +6,17 @@ print("This is plotsomething.py")
 import matplotlib.pyplot as plt
 import sys
 import argparse
-import ast
 
+# Creating the argument to grab the vector as a printed string from the script 
+# call.
 parser = argparse.ArgumentParser() 
 parser.add_argument('-list_str', nargs='*', type=str)
 
+# Grabing all arguments
 args = parser.parse_args()
-vectors = ast.literal_eval(args.list_str[0])
+
+# Evaluating the list_str argument as literal
+vectors = eval(args.list_str[0])
 print(vectors)
 
 # print(details.split(","))
